9ef568e03fb18d477307e2856c7071972a096943
shixuewen <friendlysxw@163.com>
2022-03-28 23:35:11 +0800

    add addon-a11y

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,16 @@
+import React from "react";
+import { addDecorator } from "@storybook/react";
+import { withA11y } from "@storybook/addon-a11y";
+
+import { GlobalStyle } from "../src/shared/global";
+
+addDecorator(withA11y);
+addDecorator((story) => (
+  <>
+    <GlobalStyle />
+    {story()}
+  </>
+));
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +19,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
