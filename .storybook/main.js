5d9d13353649e857ba704a0bc1721375f095923e
shixuewen <friendlysxw@163.com>
2022-03-28 19:22:21 +0800

    feat: add addons

---
+++ .storybook/main.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-28 18:49:22
+ * @LastEditTime: 2022-03-28 18:59:20
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \learnstorybook-design-system\.storybook\main.js
+ */
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-storysource",
+    "@storybook/addon-knobs",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+};
