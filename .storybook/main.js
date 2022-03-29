20b192c7d66ee1961477142c496e8b3447b42205
shixuewen <friendlysxw@163.com>
2022-03-29 12:13:57 +0800

    docs: update docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2022-03-28 18:49:22
- * @LastEditTime: 2022-03-29 00:19:02
+ * @LastEditTime: 2022-03-29 12:01:40
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \learnstorybook-design-system\.storybook\main.js
  */
 module.exports = {
-  stories: [
-    "../src/Intro.stories.mdx",
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["../src/Intro.stories.mdx", "../src/**/*.stories.(mdx|js)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-28 18:37:53
- * @LastEditTime: 2022-03-28 23:59:35
+ * @LastEditTime: 2022-03-29 11:45:18
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \learnstorybook-design-system\src\Avatar.stories.js
